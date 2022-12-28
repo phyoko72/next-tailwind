fa99391b7c8c0d821e6463d9e1e1646998bb76b8
phyokoko777 <pkkoedu777@gmail.com>
2022-12-28 17:34:02 +0630

    installed Tailwind

---
+++ pages/tailwind.js
@@ -0,0 +1,10 @@
+const Tailwind = () => {
+    return ( 
+        <>
+            <h1 className=" bg-blue-200">Hello World</h1>
+            <p className=" text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla dicta animi nam eaque molestiae. Excepturi, distinctio! Dicta sapiente nihil ducimus dolores laboriosam ad eaque sunt doloremque, iure harum hic dolor?</p>
+        </>
+     );
+}
+ 
+export default Tailwind;
